908e80bd09911f9bd27d609c802fb077ee6895a0
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-10-21 12:25:37 +0100

    Add CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import "./App.css";
+
 function App() {
   return (
     <>
-      <header>
-        <p>Food Delivery</p>
+      <header class="p-4 text-center border shadow-lg">
+        <p>
+          <span class="text-cyan-500">Food</span>Delivery
+        </p>
       </header>
-      <section></section>
-      <section></section>
+      <section class="py-12 px-4 text-center border">
+        <div class="border">
+          <h1>Food. Delivered.</h1>
+          <p>
+            Order your favourite food from local restaurants, right to you door
+          </p>
+        </div>
+      </section>
+      <section class="flex justify-center flex-wrap gap-4 mt-4">
+        <div class="w-11/12 h-16 bg-slate-500"></div>
+        <div class="w-11/12 h-16 bg-slate-500"></div>
+        <div class="w-11/12 h-16 bg-slate-500"></div>
+      </section>
       <footer>
         <p>© Copyright iO Academy 2022</p>
       </footer>
