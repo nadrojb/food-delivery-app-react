f7e8bd5b7efdc682f452b900ae8b71f31259dc8a
chris <landwaterfood@gmail.com>
2024-10-21 12:33:21 +0100

    changed border colour to match other component

--- src/Components/BtnComponent/index.jsx
+++ src/Components/BtnComponent/index.jsx
@@ -1,7 +1,7 @@
 function BtnComponent({restaurantName}) {
 
     return (
-        <button className="bg-white py-6 border-cyan-400 border-2 rounded-sm w-80 m-2 text-3xl font-medium">{restaurantName}</button>
+        <button className="bg-white py-6 border-cyan-600 border-2 rounded-sm w-80 m-2 text-3xl font-medium">{restaurantName}</button>
     )
 }
 
