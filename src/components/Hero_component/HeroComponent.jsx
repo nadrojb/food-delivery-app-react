c53de82a8f89a61c19a08a46cdcd446d3bd5d786
DBaldwin8 <darren_baldwin@live.com>
2024-10-21 15:07:48 +0100

    peer changes: remove flex, correct whitespaces, removed fragments

--- src/components/Hero_component/HeroComponent.jsx
+++ src/components/Hero_component/HeroComponent.jsx
@@ -1,11 +1,9 @@
-function HeroComponent({restaurantName}) {
+function HeroComponent() {
     return (
-      <>
-      <div className="flex flex-col items-center bg-gray-500 bg-opacity-20 max-lg:p-2 p-10 max-lg:w-11/12 w-1/2 ">
-        <h1 className=" text-cyan-500 text-5xl ">Food. Delivered.</h1>
-        <p className = " text-center max-lg:w-10/12 max-lg:pt-2 py-3">Order you favourite food from local restaurants, right to your door.</p>
-        </div>
-      </>
+      <div className="items-center bg-gray-500 bg-opacity-20 max-lg:p-2 p-10 max-lg:w-11/12 w-1/2">
+        <h1 className="text-cyan-500 text-5xl text-center">Food. Delivered.</h1>
+        <p className="text-center max-lg:pt-2 py-3">Order you favourite food from local restaurants, right to your door.</p>
+      </div>
     );
   } 
 
