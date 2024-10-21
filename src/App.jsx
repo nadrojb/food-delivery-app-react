13ca5b3a7585c81c137d861cf5e293758d948bb2
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-10-21 12:31:29 +0100

    Change all instances of class to className

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header class="p-4 text-center border shadow-lg">
+      <header className="p-4 text-center border shadow-lg">
         <p>
-          <span class="text-cyan-500">Food</span>Delivery
+          <span className="text-cyan-500">Food</span>Delivery
         </p>
       </header>
-      <section class="py-12 px-4 text-center border">
-        <div class="border">
+      <section className="py-12 px-4 text-center border">
+        <div className="border">
           <h1>Food. Delivered.</h1>
           <p>
             Order your favourite food from local restaurants, right to you door
           </p>
         </div>
       </section>
-      <section class="flex justify-center flex-wrap gap-4 mt-4">
-        <div class="w-11/12 h-16 bg-slate-500"></div>
-        <div class="w-11/12 h-16 bg-slate-500"></div>
-        <div class="w-11/12 h-16 bg-slate-500"></div>
+      <section className="flex justify-center flex-wrap gap-4 mt-4">
+        <div className="w-11/12 h-16 bg-slate-500"></div>
+        <div className="w-11/12 h-16 bg-slate-500"></div>
+        <div className="w-11/12 h-16 bg-slate-500"></div>
       </section>
       <footer>
         <p>© Copyright iO Academy 2022</p>
