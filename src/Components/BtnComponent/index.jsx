f1d8605edcb40521d1923a95d73a3bd9c47e51da
chris <landwaterfood@gmail.com>
2024-10-21 14:32:04 +0100

    peer reviewed, white space removed

--- src/Components/BtnComponent/index.jsx
+++ src/Components/BtnComponent/index.jsx
@@ -1,9 +1,5 @@
 function BtnComponent({restaurantName}) {
-
-    return (
-        <button className="bg-white py-6 border-cyan-600 border-2 rounded-sm w-80 m-2 text-3xl font-medium">{restaurantName}</button>
-    )
+    return <button className="bg-white py-6 border-cyan-600 border-2 rounded-sm w-80 m-2 text-3xl font-medium">{restaurantName}</button>
 }
-
 
 export default BtnComponent
