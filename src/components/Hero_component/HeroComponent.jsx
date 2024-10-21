e1d312cebd31e63d76086d4d4653ffb007bb1cd4
Fred_Rice <fredjrice@outlook.com>
2024-10-21 14:26:19 +0100

    Responsiveness set and background opacity set - bg grey please change to white to match

--- src/components/Hero_component/HeroComponent.jsx
+++ src/components/Hero_component/HeroComponent.jsx
@@ -1,9 +1,9 @@
-function HeroComponent({RestaurantName}) {
+function HeroComponent({restaurantName}) {
     return (
       <>
-      <div>
-        <h1 className="border border-solid border-black  ">Food.Delivered</h1>
-        <p>Order you favourite food from local restaurants, right to your door.</p>
+      <div className="flex flex-col items-center bg-gray-500 bg-opacity-20 max-lg:p-2 p-10 max-lg:w-11/12 w-1/2 ">
+        <h1 className=" text-cyan-500 text-5xl ">Food. Delivered.</h1>
+        <p className = " text-center max-lg:w-10/12 max-lg:pt-2 py-3">Order you favourite food from local restaurants, right to your door.</p>
         </div>
       </>
     );
