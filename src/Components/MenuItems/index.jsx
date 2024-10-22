e5d79891b20ce070c43ddb7c753f909433bdd41a
DBaldwin8 <darren_baldwin@live.com>
2024-10-22 15:55:32 +0100

    remove mr from itemInfo relying just on gap for spacing

--- src/Components/MenuItems/index.jsx
+++ src/Components/MenuItems/index.jsx
@@ -7,7 +7,7 @@ function MenuItems(){
         <div id="border" className="border border-solid border-black w-full mx-auto p-5 shadow-xl rounded h-40">
             <h3 id="itemName" className="font-bold text-lg">Chicken Sandwich</h3>
             <div id="itemInfo" className="flex flex-wrap pt-2 gap-1">
-                <span className="bg-cyan-500 rounded px-2 text-xs text-white h-4 mr-1">Calories: 560</span>
+                <span className="bg-cyan-500 rounded px-2 text-xs text-white h-4">Calories: 560</span>
                 <span className="bg-blue-500 rounded px-2 text-xs text-white h-4">Type: Sandwich</span>
             </div>
             <div id="priceInfo" className="flex justify-between items-center my-4 xl:my-6">
