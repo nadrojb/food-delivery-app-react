f8a87245973a865ac6b5a3a3743203ad59ba2a52
DBaldwin8 <darren_baldwin@live.com>
2024-10-22 15:58:28 +0100

    removed mx-auto from border as no change

--- src/Components/MenuItems/index.jsx
+++ src/Components/MenuItems/index.jsx
@@ -4,7 +4,7 @@ function MenuItems(){
 
     return(
 
-        <div id="border" className="border border-solid border-black w-full mx-auto p-5 shadow-xl rounded h-40">
+        <div id="border" className="border border-solid border-black w-full p-5 shadow-xl rounded h-40">
             <h3 id="itemName" className="font-bold text-lg">Chicken Sandwich</h3>
             <div id="itemInfo" className="flex flex-wrap pt-2 gap-1">
                 <span className="bg-cyan-500 rounded px-2 text-xs text-white h-4">Calories: 560</span>
