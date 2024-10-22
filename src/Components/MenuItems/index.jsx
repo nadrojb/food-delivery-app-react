e98d2e0a0ddb57152279155d5c890cc6ac1186aa
DBaldwin8 <darren_baldwin@live.com>
2024-10-22 15:54:20 +0100

    xl:my-6 added to priceInfo

--- src/Components/MenuItems/index.jsx
+++ src/Components/MenuItems/index.jsx
@@ -4,18 +4,18 @@ function MenuItems(){
 
     return(
 
-        <div id="border" className="border border-solid border-black w-full mx-auto p-5 shadow-xl rounded h-36 ">
+        <div id="border" className="border border-solid border-black w-full mx-auto p-5 shadow-xl rounded h-40">
             <h3 id="itemName" className="font-bold text-lg">Chicken Sandwich</h3>
             <div id="itemInfo" className="flex flex-wrap pt-2 gap-1">
                 <span className="bg-cyan-500 rounded px-2 text-xs text-white h-4 mr-1">Calories: 560</span>
                 <span className="bg-blue-500 rounded px-2 text-xs text-white h-4">Type: Sandwich</span>
             </div>
-            <div id="priceInfo" className="flex justify-between items-center my-2 h-8">
-                <div className="font-bold">£4.99</div>
+            <div id="priceInfo" className="flex justify-between items-center my-4 xl:my-6">
+                <div className="font-bold ">£4.99</div>
                 <div className="">
-                    <button className=" bg-blue-500 text-white h-8 w-7 rounded">-</button>
+                    <button className="bg-blue-500 text-white h-8 w-7 rounded">-</button>
                     <span className='font-extrabold text-xs p-2'>2</span>
-                    <button className=' bg-blue-500 text-white h-8 w-7 rounded'>+</button>
+                    <button className='bg-blue-500 text-white h-8 w-7 rounded'>+</button>
                 </div>
             </div>
         </div>
