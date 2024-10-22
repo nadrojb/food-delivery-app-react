d0c6f4b76660d7584059ea3cc4ae7b2fd6130978
DBaldwin8 <darren_baldwin@live.com>
2024-10-22 15:29:09 +0100

    flex wrap and gap-1  to itemInfo, lowered margin y to 2 for priceInfor.

--- src/Components/MenuItems/index.jsx
+++ src/Components/MenuItems/index.jsx
@@ -6,11 +6,11 @@ function MenuItems(){
 
         <div id="border" className="border border-solid border-black w-full mx-auto p-5 shadow-xl rounded h-36 ">
             <h3 id="itemName" className="font-bold text-lg">Chicken Sandwich</h3>
-            <div id="itemInfo" className="flex pt-2">
+            <div id="itemInfo" className="flex flex-wrap pt-2 gap-1">
                 <span className="bg-cyan-500 rounded px-2 text-xs text-white h-4 mr-1">Calories: 560</span>
                 <span className="bg-blue-500 rounded px-2 text-xs text-white h-4">Type: Sandwich</span>
             </div>
-            <div id="priceInfo" className="flex justify-between items-center  my-5 h-8">
+            <div id="priceInfo" className="flex justify-between items-center my-2 h-8">
                 <div className="font-bold">£4.99</div>
                 <div className="">
                     <button className=" bg-blue-500 text-white h-8 w-7 rounded">-</button>
