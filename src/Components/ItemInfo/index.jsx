7b15e56fd77d61d45bb5dad9da6ee82e460eb7f2
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-10-23 16:05:15 +0100

    Fix formatting and add return null to if else statement

--- src/Components/ItemInfo/index.jsx
+++ src/Components/ItemInfo/index.jsx
@@ -3,25 +3,25 @@ function ItemInfo({ itemName, itemValue }) {
   let textColour;
   let textContent;
 
-if (itemName == "calories"){
+  if (itemName == "calories") {
     backgroundColour = "bg-cyan-500";
     textColour = "text-white";
     textContent = `Calories: ${itemValue}`;
-} else if (itemName == "foodType"){
+  } else if (itemName == "foodType") {
     backgroundColour = "bg-blue-500";
     textColour = "text-white";
     textContent = `Type: ${itemValue}`;
-} else if (itemName == "sideItem"){
+  } else if (itemName == "sideItem") {
     backgroundColour = "bg-yellow-400";
     textColour = "text-black";
     textContent = "Side";
-}
+  } else {
+    return null;
+  }
 
   return (
     <div className={`${backgroundColour} rounded px-2 text-xs`}>
-      <p className={`${textColour} text-xs font-semibold`}>
-        {textContent}
-      </p>
+      <p className={`${textColour} text-xs font-semibold`}>{textContent}</p>
     </div>
   );
 }
